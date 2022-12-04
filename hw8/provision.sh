d3995dbe5f08600d7a0e248b66e516db27a1b000
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2022-12-05 00:11:50 +0200

    hw8 done

---
+++ hw8/provision.sh
@@ -0,0 +1,11 @@
+sudo mkdir -p /var/www/localhost/ && sudo touch /var/www/localhost/index.html
+sudo cat /home/vagrant/www/index.html > /var/www/localhost/index.html
+sudo mkdir -p "/etc/httpd/conf.d/"
+sudo cat /home/vagrant/www/localhost.conf > /etc/httpd/conf.d/localhost.conf
+
+chcon -Rt httpd_sys_content_t /var/www/localhost/
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/CN=localhost/C=UA/L=Lviv/O=VPovstenko"
+sudo yum install -y epel-release httpd mod_ssl
+sudo httpd -t
+sudo systemctl start httpd
